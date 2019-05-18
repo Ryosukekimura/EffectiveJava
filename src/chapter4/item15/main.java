a1de7c1d1b1e51fe30d7dde243c7b1562bcab6f6
k1449 <k1449@DESKTOP-GI9M2IQ>
2019-05-19 01:58:07 +0900

    練習コミット

---
+++ src/chapter4/item15/main.java
@@ -0,0 +1,14 @@
+package chapter4.item15;
+
+/**
+ * 検証クラス
+ * @author k1449
+ *
+ */
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
